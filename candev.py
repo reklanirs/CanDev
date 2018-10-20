0f3d61835a8374699842d878b3b015c1ef8aa2ab
reKlanirs <tiansuixing@gmail.com>
2018-10-20 17:00:08 -0400

    2b-1 is finished

--- candev.py
+++ candev.py
@@ -4,6 +4,7 @@ import os,re,sys,time
 import math,random
 import csv
 import numpy as np
+import re
 
 
 clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
@@ -24,6 +25,7 @@ def load_dataset(f = file_path):
     print(header)
     print('Total lines in this dataset:', len(data))
     print('A sample line:', data[-1])
+    
 
 
 def _2b_1():
@@ -31,14 +33,22 @@ def _2b_1():
     2. valuable
     In the case of self-describing digital datasets, is the format either JSON (preferred) or XML-based using a well-known schema (or accompanied by the schema employed)?
     '''
-    # Your code here
-    
-    return Answer[0],'No one in dataset conflict against this checklist'
+    ans, reason = 1,''
+    with open(file_path, 'r') as unknown_file:
+        # Remove tabs, spaces, and new lines when reading
+        data = re.sub(r'\s+', '', unknown_file.read())
+        if (re.match(r'^<.+>$', data)):
+            ans,reason = 0, 'This file is XML'
+        if (re.match(r'^({|[).+(}|])$', data)):
+            ans, reason = 0, 'This file is JSON'
+        ans, reason = 1, 'This file is either not XML nor JSON'
+    return Answer[ans], reason
 
 
 
 def main():
     load_dataset()
+    print(_2b_1())
     pass
 
 if __name__ == '__main__':
