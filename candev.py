4cc298d443af9fc6224e635ecac6f199a475ef29
Lily Huang <390530394@qq.com>
2018-10-20 22:33:30 -0400

    16，17

--- candev.py
+++ candev.py
@@ -24,7 +24,7 @@ def load_dataset(f = file_path):
             data.append(row[:-1])
     print(header)
     print('Total lines in this dataset:', len(data))
-    print('A sample line:', data[-1])
+    print('A sample line:', data[0])
     
 
 
@@ -157,45 +157,45 @@ def _2b_15():
     #Your code here
 
     return Answer[ans], reason
+
 def _2b_16():
     '''
+    2. valuable
+    Are the filenames consistent, descriptive, and informative (clearly indicates content) to humans?
     '''
-    ans, reason = 1,''
-    #Your code here
-
+    ans, reason = 0,'The question is subjective, so it would be better to double check it by humans.'
     return Answer[ans], reason
 
-
-def _2b_():
-    '''
-    '''
-    ans, reason = 1,''
-    #Your code here
-
-    return Answer[ans], reason
 def _2b_17():
     '''
+    3. desirable
+    Do the filenames follow the convention: less than 70 characters; most unique content at start of filename; no acronyms; no jargon; no organization name?
     '''
-    ans, reason = 1,''
-    #Your code here
-
+    ans, reason = 0, ''
+    filename = os.path.splitext(file_path)[-2]
+    if(len(filename)>70):
+        ans, reason = 1, 'File name should not be longer than 70 characters.'
+    else:
+        #if there are two or more continuous capitals, there might be acronyms in file name
+        has_continuous_capital = False
+        for i in range(1,len(filename)):
+            if filename[i].isupper() and filename[i-1].isupper():
+                has_continuous_capital = True
+                break
+        if has_continuous_capital == True:
+            ans, reason = 1, 'There might be acronyms in file name.'
     return Answer[ans], reason
 
 
 def _2b_18():
-    '''
-    '''
-    ans, reason = 1,''
-    #Your code here
 
     return Answer[ans], reason
-
 
 
 
 def main():
     load_dataset()
-    print(_2b_1())
+    print(_2b_17())
     pass
 
 if __name__ == '__main__':
